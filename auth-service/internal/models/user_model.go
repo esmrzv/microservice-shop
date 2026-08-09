b51a7f651f97a4f6d842583de8a1040fd8d82560
esmrzv <esmurziev.1999@mail.ru>
2026-08-09 22:23:27 +0300

    feat: add model user

---
+++ auth-service/internal/models/user_model.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"github.com/google/uuid"
+
+)
+
+
+
+type User struct {
+	ID uuid.UUID
+	Email string
+	PasswordHash string
+}
+
+type UserCreate struct{
+	Email string
+	Password string
+}
