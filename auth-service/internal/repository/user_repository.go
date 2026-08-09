ae393660a3e8822c942152c273fd3245e33bced3
esmrzv <esmurziev.1999@mail.ru>
2026-08-09 22:23:52 +0300

    feat: add auth repository

---
+++ auth-service/internal/repository/user_repository.go
@@ -0,0 +1,61 @@
+package repository
+
+import (
+	"context"
+	"errors"
+
+	"github.com/esmrzv/microservice-shop/auth-service/internal/models"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+	
+
+
+type UserRepository interface{
+	CreateUser(ctx context.Context, user *models.User) error
+	GetByEmail(ctx context.Context, email string) (*models.User, error)
+}
+
+var ErrUserNotFound = errors.New("user not found")
+
+type userRepository struct {
+	db *pgxpool.Pool
+	
+}
+
+
+func NewUserRepository(db *pgxpool.Pool) UserRepository{
+	return &userRepository{db: db}
+
+}
+
+func (r *userRepository) CreateUser(ctx context.Context, user *models.User) error {
+	query := `
+		INSERT INTO users(email, password_hash)
+		VALUES($1, $2)
+		RETURNING id
+	`
+	err := r.db.QueryRow(ctx, query, 
+		user.Email,
+		user.PasswordHash,
+	).Scan(&user.ID)
+	return err
+
+}
+
+func (r *userRepository) GetByEmail(ctx context.Context, email string) (*models.User, error){
+	query := `
+		SELECT * FROM users
+		WHERE email= $1;
+	`
+	var user models.User
+	err := r.db.QueryRow(ctx, query, email).Scan(&user.ID, &user.Email)
+	if err != nil{
+		if errors.Is(err, pgx.ErrNoRows){
+			return nil, ErrUserNotFound
+		}
+		return nil, err
+	}
+	return &user, nil
+	
+}
