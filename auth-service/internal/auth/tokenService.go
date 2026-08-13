dbd2d916521cc1d4c607d4e71f1d56d7e657364b
esmrzv <esmurziev.1999@mail.ru>
2026-08-13 12:04:55 +0300

    feat: fix tokenService.go

--- auth-service/internal/auth/tokenService.go
+++ auth-service/internal/auth/tokenService.go
@@ -7,11 +7,9 @@ import (
 	"github.com/google/uuid"
 )
 
-
 type TokenService interface {
 	Generate(userID uuid.UUID) (string, error)
 }
-
 
 type tokenService struct {
 	secret []byte
@@ -23,16 +21,15 @@ func NewTokenService(secret string) TokenService {
 	}
 }
 
-
 func (s *tokenService) Generate(userID uuid.UUID) (string, error) {
 	claims := jwt.MapClaims{
 		"user_id": userID.String(),
-		"iat": time.Now().Unix(),
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
+		"iat":     time.Now().Unix(),
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	signedToken, err := token.SignedString(s.secret)
-	if err != nil{
+	if err != nil {
 		return "", err
 	}
 	return signedToken, nil
