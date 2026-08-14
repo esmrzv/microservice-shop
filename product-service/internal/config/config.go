9925b2b8a48e8be9a9f23eb417790960855b967d
esmrzv <esmurziev.1999@mail.ru>
2026-08-14 20:31:59 +0300

    feat: add config product-service

---
+++ product-service/internal/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	HTTPPort string
+
+	DBHost string
+	DBPort string
+
+	DBUser     string
+	DBPassword string
+
+	DBName string
+}
+
+func Load() (*Config, error) {
+	_ = godotenv.Load()
+	cfg := &Config{
+		HTTPPort:   os.Getenv("HTTP_PORT"),
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASS"),
+		DBName:     os.Getenv("DB_NAME"),
+	}
+	return cfg, nil
+}
