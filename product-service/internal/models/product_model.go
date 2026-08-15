0e67ef8a6df036027333b6ad554ce5c4332c5d5e
esmrzv <esmurziev.1999@mail.ru>
2026-08-15 14:56:53 +0300

    refactor: change string> uuid

--- product-service/internal/models/product_model.go
+++ product-service/internal/models/product_model.go
@@ -1,9 +1,10 @@
 package models
 
 import "time"
+import "github.com/google/uuid"
 
 type Product struct {
-	ID          string
+	ID          uuid.UUID
 	Name        string
 	Description string
 	Price       float64
