9452e92b6b33a7b277d3675bd5b32e1fbf453ada
esmrzv <esmurziev.1999@mail.ru>
2026-08-17 22:20:09 +0300

    feat: add main product service

---
+++ product-service/cmd/api/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/esmrzv/product-service/internal/config"
+	"github.com/esmrzv/product-service/internal/database"
+	"github.com/esmrzv/product-service/internal/handler"
+	apiHTTP "github.com/esmrzv/product-service/internal/http"
+	"github.com/esmrzv/product-service/internal/repository"
+	"github.com/esmrzv/product-service/internal/service"
+)
+
+
+
+func main(){
+	cfg, err := config.Load()
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+		)
+	defer stop()
+
+	db, err := database.NewPostgres(ctx, cfg)
+	if err != nil{
+		log.Fatal("error connect to db")
+	}
+	defer db.Close()
+
+	productRepo := repository.NewProductRepository(db)
+	productService := service.NewProductService(productRepo)
+	productHandler := handler.NewProductHandler(productService)
+	productRouter := apiHTTP.NewProductRouter(productHandler)
+
+	server := &http.Server{
+		Addr: cfg.HTTPPort,
+		Handler: productRouter,
+	}
+
+	go func ()  {
+		log.Printf("server starting in port :%s", cfg.HTTPPort)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed){
+				log.Printf("Listen: %s\n", err)
+		}
+	}()
+		
+	<- ctx.Done()
+
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctxShutDown); err != nil{
+		log.Printf("error shutDown %s\n", err)
+	}
+		
+}
