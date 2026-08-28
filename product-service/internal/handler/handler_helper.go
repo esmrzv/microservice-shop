a1381a142e77b86cbab991f0712440e0e33b8727
esmrzv <esmurziev.1999@mail.ru>
2026-08-28 19:49:15 +0300

    feat: add method get for getAllProducts

--- product-service/internal/handler/handler_helper.go
+++ product-service/internal/handler/handler_helper.go
@@ -6,16 +6,15 @@ import (
 	"github.com/esmrzv/product-service/internal/service"
 )
 
-
-func setupHandlerRoute(service service.ProductService) *http.ServeMux{
+func setupHandlerRoute(service service.ProductService) *http.ServeMux {
 	handler := NewProductHandler(service)
 	mux := http.NewServeMux()
+	mux.HandleFunc("GET /products", handler.GetAllProducts)
 	mux.HandleFunc("GET /products/{id}", handler.GetProductByID)
 	mux.HandleFunc("POST /products", handler.CreateProduct)
 	mux.HandleFunc("PUT /products/{id}", handler.UpdateProduct)
-    mux.HandleFunc("DELETE /products/{id}", handler.DeleteProduct)
+	mux.HandleFunc("DELETE /products/{id}", handler.DeleteProduct)
 
-    return mux
-
+	return mux
 
 }
