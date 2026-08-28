180ac44fe867c5eb405152ad14d68f352d8b0a19
esmrzv <esmurziev.1999@mail.ru>
2026-08-28 14:55:44 +0300

    feat: add sutupHandler for product handler test

---
+++ product-service/internal/handler/handler_helper.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/esmrzv/product-service/internal/service"
+)
+
+
+func setupHandlerRoute(service service.ProductService) *http.ServeMux{
+	handler := NewProductHandler(service)
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /products/{id}", handler.GetProductByID)
+	mux.HandleFunc("POST /products", handler.CreateProduct)
+	mux.HandleFunc("PUT /products/{id}", handler.UpdateProduct)
+    mux.HandleFunc("DELETE /products/{id}", handler.DeleteProduct)
+
+    return mux
+
+
+}
